3e683552cbceeb5890cb14aaf89ed04bd99f7e40
Keaton Anthony Murray <murraykeaton422@gmail.com>
2024-10-26 10:27:50 -0400

    removed all project folders

--- resources/views/components/flex-centered-column-component.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="row">
-    <div id= {{ $id }} class="d-flex justify-content-center align-items-center" {{ $additionalClass }}>
-        {{ $slot }}
-    </div>
-</div>
--- resources/views/components/form-input-component.blade.php
+++
@@ -1,2 +0,0 @@
-<label class="form-label mt-2">{{$label}}</label>
-<input type="{{$type}}" name="{{ $name }}" class="form-control line-input mb-2">
